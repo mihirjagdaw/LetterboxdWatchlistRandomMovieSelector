4156f20c31b2ce08fb06d446038b160b49c4541d
ST10449392 <162103573+mihirjagdaw@users.noreply.github.com>
2026-02-19 09:36:09 +0200

    Encapsulated logic into MovieSelector.cs

---
+++ MovieSelector.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LetterboxdWatchlistRandomMovieSelector
+{
+    internal class MovieSelector
+    {
+        // Dictionary to store movie names and years
+        private Dictionary<string, string> movies = new Dictionary<string, string>();
+
+        // Constructor to read the CSV file and populate the movies dictionary
+        public MovieSelector()
+        {
+            using (var reader = new StreamReader(@"C:\\Users\\mihir\\Downloads\\watchlist-fortnutmastr006-2026-02-15-16-24-utc.csv"))
+            {
+                var values = new string[4];
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    values = line.Split(',');
+                    movies.Add(values[1], values[2]);
+
+                }
+            }
+        }
+
+        // Method to generate a random movie and print its torrent link
+        public void generateRandomMovie()
+        {
+            var random = new Random();
+            int randomIndex = random.Next(0, movies.Count);
+            var randomMovie = movies.ElementAt(randomIndex).Key;
+            var movieYear = movies.ElementAt(randomIndex).Value;
+
+            string movieName = randomMovie.Replace(" ", "-").ToLower();
+
+            // Print the randomly selected movie and its torrent link
+            Console.WriteLine($"Randomly selected movie: {randomMovie}");
+
+            Console.WriteLine($"Torrent Link: https://yts.bz/movies/{movieName}-{movieYear}");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -12,28 +12,8 @@ namespace LetterboxdWatchlistRandomMovieSelector
     {
         static void Main(string[] args)
         {
-            using (var reader = new StreamReader(@"C:\\Users\\mihir\\Downloads\\watchlist-fortnutmastr006-2026-02-12-07-49-utc.csv"))
-            {
-                Dictionary <string, string> movies = new Dictionary<string, string>();
-                var values = new string[4];
-                while (!reader.EndOfStream)
-                {
-                    var line = reader.ReadLine();
-                    values = line.Split(',');
-                    movies.Add(values[1], values[2]);
-
-                }
-                var random = new Random();
-                int randomIndex = random.Next(0, movies.Count);
-                var randomMovie = movies.ElementAt(randomIndex).Key;
-                var movieYear = movies.ElementAt(randomIndex).Value;
-                Console.WriteLine($"Randomly selected movie: {randomMovie}");
-
-                string movieName = randomMovie.Replace(" ", "-").ToLower();
-                
-                Console.WriteLine($"Torrent Link: https://yts.bz/movies/{movieName}-{movieYear}");
-            }
-
+            MovieSelector movieSelector = new MovieSelector();
+            movieSelector.generateRandomMovie();
         }
     }
 }
